b6b8e470dde25e636b1d1e8a689e692a6f07cb80
namanmanchanda09 <naman.mohan09@outlook.com>
2019-10-01 13:50:40 +0530

    Updates

---
+++ precache-manifest.3c233a601ad3825f9b6f40bf1d168e4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eba15e1f6748575980e92f1a4da5520",
+    "url": "/techdaddy/index.html"
+  },
+  {
+    "revision": "5d38dc7371230059082d",
+    "url": "/techdaddy/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "4f85ba5986cce6b071cb",
+    "url": "/techdaddy/static/css/main.9457c000.chunk.css"
+  },
+  {
+    "revision": "5d38dc7371230059082d",
+    "url": "/techdaddy/static/js/2.e5b900e0.chunk.js"
+  },
+  {
+    "revision": "4f85ba5986cce6b071cb",
+    "url": "/techdaddy/static/js/main.9499f267.chunk.js"
+  },
+  {
+    "revision": "c71a70f61c8eced5c86e",
+    "url": "/techdaddy/static/js/runtime-main.ddbfa21a.js"
+  },
+  {
+    "revision": "30a555ccf5a2917ad223d74b22a61cef",
+    "url": "/techdaddy/static/media/python.30a555cc.png"
+  }
+]);
